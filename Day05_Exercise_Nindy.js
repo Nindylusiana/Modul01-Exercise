304015840d68d3e25fe6c908f339cf2b06b4752a
Nindylusiana <nindysimanullang@gmail.com>
2022-11-08 01:08:14 +0700

    exercise 5

---
+++ Day05_Exercise_Nindy.js
@@ -0,0 +1,74 @@
+// lowest, highest, avg
+
+// let number = [2, 5, 8, 1, 9];
+// let max = number[0];
+
+// for (let i in number) {
+//   if (number[i] > max) {
+//     max = number[i];
+//   }
+// }
+
+// let number = [2, 5, 8, 1, 9];
+// let min = number[0];
+
+// for (let i in number) {
+//   if (number[i] < min) {
+//     min = number[i];
+//   }
+// }
+
+// let number = [2, 5, 8, 1, 9];
+
+// let sum = 0;
+// for (i in number) {
+//   sum += number[i];
+// }
+// let avg = sum / number.length;
+
+// console.log(`nilai tertinggi adalah ${max}`);
+// console.log(`nilai tertinggi adalah ${min}`);
+// console.log(`nilai tertinggi adalah ${avg}`);
+
+// concatenating
+
+// let string = (arr) => {
+//   let last = arr.pop();
+//   return `${arr.join(", ")} and ${last}`;
+// };
+// const arrResult = ["anggur", "appel", "jeruk"];
+// console.log(string(arrResult));
+
+// penjumlahan isi dua array
+
+// const integer = (a, b = 0) => {
+//   return a.map((num, i) => {
+//     return num + b[i];
+//   });
+// };
+
+// const int1 = [1, 2, 3];
+// const int2 = [3, 2, 1];
+
+// console.log(integer(int1, int2));
+
+// split string
+
+// let split = (str) => {
+//   return str.trim().split(" ");
+// };
+// console.log(split("helo word"));
+
+//remove odd number
+
+// let array = [1, 3, 5, 7, 9, 2, 4, 6, 8];
+
+// for (let i = 0; i < array.length; i++) {
+//   for (let j = 0; j < array.length; j++)
+//     if (array[j] % 2 === 0) {
+//       array.splice(j);
+//       break;
+//     }
+// }
+
+// console.log(array);
