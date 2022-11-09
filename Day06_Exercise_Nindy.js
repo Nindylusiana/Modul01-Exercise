4e2f7c1e33b834fccf254d08b091f59c9ab356d5
Nindylusiana <nindysimanullang@gmail.com>
2022-11-10 02:16:27 +0700

    exercise 6

---
+++ Day06_Exercise_Nindy.js
@@ -0,0 +1,57 @@
+//EXERCISE 1
+// object
+// let student = [
+//   { name: "nindy", email: "nindylusiana@gmail.com", age: 20, score: 80 },
+//   { name: "dinda", email: "dinda@gmail.com", age: 23, score: 78 },
+//   { name: "dewi", email: "dewi@gmail.com", age: 21, score: 83 },
+// ];
+//age
+// function maxAge(data) {
+//   return (resultMax = data.reduce((min, p) => (p.age > min ? p.age : min), data[0].age));
+// }
+// function minAge(data) {
+//   return (resultMax = data.reduce((min, p) => (p.age < min ? p.age : min), data[0].age));
+// }
+
+// score
+// function maxScore(data) {
+//   return (resultMax = data.reduce((min, p) => (p.score > min ? p.score : min), data[0].score));
+// }
+// function minScore(data) {
+//   return (resultMin = data.reduce((min, p) => (p.score < min ? p.score : min), data[0].score));
+// }
+
+// avg score
+// var arr = student.map((obj) => obj.score);
+
+// let avgScore = 0;
+// for (i = 0; i < arr.length; i++) {
+//   avgScore += arr[i];
+// }
+// let hasil = Math.floor(avgScore / arr.length);
+
+//avg age
+// var arr = student.map((obj) => obj.age);
+
+// let avgAge = 0;
+// for (i = 0; i < arr.length; i++) {
+//   avgAge += arr[i];
+// }
+// let hasilAge = Math.floor(avgAge / arr.length);
+
+// console.log(hasilAge);
+// console.log(hasil);
+// console.log("highest score: ", maxScorestudent));
+// console.log("lowest score: ", minScore(student));
+// console.log("highest age: ", maxAge(student));
+// console.log("lowest age: ", minAge(student));
+
+//EXERCISE 2
+//class
+
+// class Product {
+//   constructor(name, price) {
+//     this.name = name;
+//     this.price = price;
+//   }
+// }
